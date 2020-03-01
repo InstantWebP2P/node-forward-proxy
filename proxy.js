650c7451936afff1d4fb6b40a712b0616a3e311c
tom zhou <iwebpp@gmail.com>
2020-02-29 19:48:05 -0800

    remove iwebpp.com

--- proxy.js
+++ proxy.js
@@ -77,11 +77,11 @@ var Proxy = module.exports = function(options, fn){
         srvinfo: {
             timeout: 20,
             endpoints: (options && options.endpoints) || [
-                {ip: 'iwebpp.com', port: 51686},
-                {ip: 'iwebpp.com', port: 51868}
+                {ip: 'httpp.aiworkspace.com', port: 51686},
+                {ip: 'httpp.aiworkspace.com', port: 51868}
             ],
             turn: (options && options.turn) || [
-                {ip: 'iwebpp.com', agent: 51866, proxy: 51688}
+                {ip: 'httpp.aiworkspace.com', agent: 51866, proxy: 51688}
             ]
         },
 
@@ -306,8 +306,8 @@ var Proxy = module.exports = function(options, fn){
 		    		    
 		    // 1.
 		    // match vURL pattern:
-		    // - vhost like http(s)://xxx.vurl.iwebpp.com
-		    // - vpath like http(s)://iwebpp.com"/vurl/xxx"
+		    // - vhost like http(s)://xxx.vurl.httpp.aiworkspace.com
+		    // - vpath like http(s)://httpp.aiworkspace.com"/vurl/xxx"
 		    if (vstrs = req.headers.host.match(vURL.regex_vhost)) {
 		        vurle = vstrs[0];
 		        if (Debug) console.log('proxy for client with vhost:'+vurle);
@@ -611,8 +611,8 @@ var Proxy = module.exports = function(options, fn){
 		    
 		    // 1.
 		    // match vURL pattern:
-		    // - vhost like http(s)://xxx.vurl.iwebpp.com
-		    // - vpath like http(s)://iwebpp.com/vurl/xxx"
+		    // - vhost like http(s)://xxx.vurl.httpp.aiworkspace.com
+		    // - vpath like http(s)://httpp.aiworkspace.com/vurl/xxx"
 		    if (vstrs = urle.match(vURL.regex_vhost)) {
 		        vurle = vstrs[0];
 		        if (Debug) console.log('tunnel for client with vhost:'+vurle);
